3b28236891fd3f956fe1a69e502f950f8ddcc88e
Michael Lynn <merlynn@gmail.com>
2021-02-25 10:38:48 -0500

    Add Update One Document Example

---
+++ update-one-document.php
@@ -0,0 +1,19 @@
+<?php
+
+      require_once __DIR__ . '/vendor/autoload.php';
+      $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+      $dotenv->load();
+
+      $client = new MongoDB\Client(
+         'mongodb+srv://'.$_ENV['MDB_USER'].':'.$_ENV['MDB_PASS'].'@'.$_ENV['ATLAS_CLUSTER_SRV'].'/sample_restaurants'
+      );
+     
+      $collection = ($client)->sample_restaurants->restaurants;
+
+      $updateResult = $collection->updateOne(
+         [ 'restaurant_id' => '40356151' ],
+         [ '$set' => [ 'name' => 'Brunos on Astoria' ]]
+      );
+
+      printf("Matched %d document(s)\n", $updateResult->getMatchedCount());
+      printf("Modified %d document(s)\n", $updateResult->getModifiedCount());
